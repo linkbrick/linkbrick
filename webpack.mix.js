4860e08703c7c510ba34a772f8eae77fd77bb4b7
Eng <normalperson85@gmail.com>
2018-03-30 13:51:40 +0000

    migrate from proptech to linkbrick

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,35 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .scripts('resources/assets/js/vue_init.js'
+        , 'public/js/vue_init.js')
+    .scripts(['resources/assets/paperkit/js/jquery-3.2.1.min.js',
+    'resources/assets/paperkit/js/jquery-ui-1.12.1.custom.min.js',
+    'resources/assets/paperkit/js/popper.js',
+    'resources/assets/paperkit/js/bootstrap.min.js',
+    'resources/assets/paperkit/js/paper-kit.js'
+    ],'public/js/paper-kit.js')
+    .scripts([
+        'resources/assets/material-dashboard/js/jquery-3.2.1.min.js',
+        'resources/assets/material-dashboard/js/bootstrap.min.js',
+        'resources/assets/material-dashboard/js/material.min.js',
+        'resources/assets/material-dashboard/js/perfect-scrollbar.jquery.min.js',
+        'resources/assets/material-dashboard/js/arrive.min.js',
+        'resources/assets/material-dashboard/js/jquery.validate.min.js',
+        'resources/assets/material-dashboard/js/moment.min.js',
+        'resources/assets/material-dashboard/js/jquery.bootstrap-wizard.js',
+        'resources/assets/material-dashboard/js/bootstrap-notify.js',
+        'resources/assets/material-dashboard/js/bootstrap-datetimepicker.js',
+        'resources/assets/material-dashboard/js/nouislider.min.js',
+        'resources/assets/material-dashboard/js/jquery.select-bootstrap.js',
+        'resources/assets/material-dashboard/js/sweetalert2.js',
+        'resources/assets/material-dashboard/js/jasny-bootstrap.min.js',
+        'resources/assets/material-dashboard/js/jquery.tagsinput.js'
+    ], 'public/js/material.js')
+    .scripts('resources/assets/material-dashboard/js/material-dashboard.js'
+        , 'public/js/material-dashboard.js')
+    .scripts('resources/assets/material-dashboard/js/fullcalendar.min.js'
+        , 'public/js/fullcalendar.js')
+    .sass('resources/assets/material-dashboard/sass/material-dashboard.scss', 'public/css')
+    .sass('resources/assets/paperkit/sass/paper-kit.scss', 'public/css')
+    .sass('resources/assets/paperkit/sass/demo.scss', 'public/css');
