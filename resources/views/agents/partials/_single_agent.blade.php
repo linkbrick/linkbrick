fd77931443b05d86fc3541613facd18d6fcf13f9
Eng <normalperson85@gmail.com>
2018-04-20 02:59:13 +0800

    finish draft the agent page

--- resources/views/agents/partials/_single_agent.blade.php
+++ resources/views/agents/partials/_single_agent.blade.php
@@ -1,7 +1,51 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: esp
- * Date: 22/04/2018
- * Time: 4:21 PM
- */
+<div class="modal fade" id="agentModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-notice">
+        <div class="modal-content">
+            <div class="modal-header no-border-header">
+                <h5 class="modal-title" id="myModalLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            </div>
+            <div class="modal-body">
+                <div class="instruction">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <p> <strong>1. Register</strong> - the first step is to create an account at <a href="http://www.creative-tim.com/">Creative Tim</a>. You can choose a social network or go for the classic version, whatever works best for you.</p>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="picture">
+                                <img src="assets/img/sections/angelo-pantazis.jpg" alt="Thumbnail Image"  class="img-rounded img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="instruction">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <p> <strong>2. Apply</strong> - the first step is to create an account at <a href="http://www.creative-tim.com/">Creative Tim</a>. You can choose a social network or go for the classic version, whatever works best for you.</p>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="picture">
+                                <img src="assets/img/sections/rawpixel-coms.jpg" alt="Thumbnail Image"  class="img-rounded img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <p>If you have more questions, don't hesitate to contact us or send us a tweet @creativetim. We're here to help!</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary btn-link" data-dismiss="modal">Okay</button>
+            </div>
+        </div>
+    </div>
+</div>
+@push('scripts')
+<script>
+    $(document).on("click", ".open-agentModal", function () {
+        var agentName = $(this).data('agent-name');
+        $(".modal-content .modal-title").val( agentName );
+        // As pointed out in comments,
+        // it is superfluous to have to manually call the modal.
+        // $('#addBookDialog').modal('show');
+    });
+</script>
+@endpush
--- resources/views/agents/team.blade.php
+++ resources/views/agents/team.blade.php
@@ -80,9 +80,22 @@
                 @foreach($agents->where('type', 'agent')->chunk(4) as $chunk)
                     @foreach($chunk as $akey => $agent)
                         <div class="col-md-3">
-                            <div class="card card-profile card-plain">
+                            <div class="card card-profile card-plain"
+
+
+                            >
                                 <div class="card-avatar">
-                                    <a href="#avatar"><img src="{{ $agent->getMedia('agent_image')->first()->getUrl()  }}" alt="..."></a>
+                                    <a href="javascript:void(0)"
+                                       class="agent"
+                                       data-agent-name="{{ $agent->name }}"
+                                       data-agent-image-url="{{ $agent->getMedia('agent_image')->first()->getUrl() }}"
+                                       data-agent-position="{{ $agent->position }}"
+                                       data-agent-email="{{ $agent->email }}"
+                                       data-agent-excerpt="{{ $agent->excerpt }}"
+                                       data-agent-facebook_url="{{ $agent->facebook_url }}"
+                                       data-agent-instagram_url="{{ $agent->instagram_url }}"
+                                       data-agent-description="{!! $agent->description !!} "
+                                    ><img src="{{ $agent->getMedia('agent_image')->first()->getUrl()  }}" alt="..."></a>
                                 </div>
                                 <div class="card-body">
                                     <a href="#paper-kit">
@@ -108,3 +121,70 @@
     </div>
     {{--End Of Team Member--}}
 @endsection
+
+
+<div class="modal fade" id="agentModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-notice">
+        <div class="modal-content">
+            <div class="modal-header no-border-header">
+                <h5 class="modal-title" id="agent_name"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            </div>
+            <div class="modal-body">
+                <div class="instruction">
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="picture">
+                                <img id="agent_image" src="assets/img/sections/angelo-pantazis.jpg" alt="Thumbnail Image"  class="img-rounded img-responsive">
+                            </div>
+                        </div>
+
+
+                        <div class="col-md-6">
+                            <p id="agent_excerpt"></p>
+                            <p id="agent_email">
+                            </p>
+                            <a id="agent_facebook" href="#" class="btn btn-link btn-just-icon btn-facebook"><i class="fa fa-facebook"></i></a>
+                            <a id="agent_instagram" href="#" class="btn btn-link btn-just-icon btn-instagram"><i class="fa fa-instagram"></i></a>
+                        </div>
+
+                    </div>
+                </div>
+                <br/>
+                <div class="instruction">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <p id="agent_description"></p>
+                        </div>
+
+                    </div>
+                </div>
+                <p>If you have more questions, don't hesitate to contact us or send us a tweet @creativetim. We're here to help!</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary btn-link" data-dismiss="modal">Okay</button>
+            </div>
+        </div>
+    </div>
+</div>
+@push('scripts')
+    <script>
+
+        $( document ).ready(function() {
+            $('.agent').on('click',function(){
+                $this = $(this);
+                $('#agent_name').html($this.data('agent-name'));
+                $('#agent_image').attr("src",$this.data('agent-image-url'));
+                $('#agent_excerpt').html($this.data('agent-excerpt'));
+                $('#agent_email').html($this.data('agent-email'));
+                $('#agent_description').html($this.data('agent-description'));
+                $('#agent_facebook').attr('href',$this.data('agent-facebook_url'));
+                $('#agent_instagram').attr('href',$this.data('agent-instagram_url'));
+                $('#agentModal').modal('show');
+            });
+
+        });
+
+    </script>
+@endpush
