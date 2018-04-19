f9e7723eacdd55fb837a45a6b6ae14ff6d10f08e
Eng <normalperson85@gmail.com>
2018-04-19 23:56:13 +0800

    working on agent CRUD

---
+++ database/migrations/2018_04_19_154324_create_agents_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAgentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->nullable()->index();
+            $table->string('name');
+            $table->string('position');
+            $table->string('excerpt');
+            $table->text('description');
+            $table->string('email');
+            $table->string('contact_no');
+            $table->string('type');
+            $table->integer('sequence');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agents');
+    }
+}
